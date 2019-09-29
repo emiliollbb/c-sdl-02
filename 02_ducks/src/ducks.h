caa3b697fcd9496caadded01f4f66fb31b770efc
Emilio Lopez <emiliollbb@gmail.com>
2019-09-29 12:16:22 +0200

    Empty ducks

---
+++ 02_ducks/src/ducks.h
@@ -0,0 +1,19 @@
+#include <SDL2/SDL_ttf.h>
+#include "../../01_pong/src/main.h"
+
+#ifndef PONG_H
+#define PONG_H
+
+#define DUCKS_SIZE 20
+
+struct ducks_media_s {
+
+};
+
+struct ducks_game_data_s {
+	struct kinematics_struct hunter;
+	struct kinematics_struct ducks[DUCKS_SIZE];
+	struct ducks_media_s media;
+};
+
+#endif
